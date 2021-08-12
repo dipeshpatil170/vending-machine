9f5e9ba7fa212359cf85c51a5802ea84efdab335
dipeshpatil170 <30872091+dipeshpatil170@users.noreply.github.com>
2021-08-12 13:44:58 +0530

    added icons

--- src/pages/balancePannel.tsx
+++ src/pages/balancePannel.tsx
@@ -1,7 +1,14 @@
+import { Box, Text } from '@chakra-ui/react'
 import React from 'react'
 
 interface Props {}
 
 export const BalancePannel = (props: Props) => {
-   return <div>BalancePannel</div>
+   return (
+      <>
+         <Box w="100%" p={4} color="black" bg={"white"} border="1px solid gray" borderBottomEndRadius={10} borderBottomStartRadius={10}>
+            <Text fontSize="lg">Available Balance : 500 INR</Text>
+         </Box>
+      </>
+   )
 }
